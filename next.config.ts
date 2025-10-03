49d4a8d54d7ee2ad7b3ba268b2932860b6cdd1b9
= <=>
2025-10-03 20:59:46 -0300

    configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/NOME-DO-SEU-REPOSITORIO' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/NOME-DO-SEU-REPOSITORIO/' : '',
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
