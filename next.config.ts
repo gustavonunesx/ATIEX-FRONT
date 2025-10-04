e1c6b5937b1b3789342e084c19d2b83c45fef29b
= <=>
2025-10-03 21:26:09 -0300

    modificacoes

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,11 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  distDir: 'out',
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/ATIEX-FRONT' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ATIEX-FRONT/' : '',
+  basePath: '/ATIEX-FRONT',
+  assetPrefix: '/ATIEX-FRONT/',
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
