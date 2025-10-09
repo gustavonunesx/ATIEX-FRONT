57bf2c233cc35d07758787b39dd0b676120b783f
= <gu.nunesx@gmail.com>
2025-10-08 21:18:32 -0300

    att

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  skipTrailingSlashRedirect: true,
   images: {
     unoptimized: true
   },
-  basePath: '/ATIEX-FRONT',
-  assetPrefix: '/ATIEX-FRONT', //dqwdqw
+
 }
 
-module.exports = nextConfig;
+export default nextConfig
