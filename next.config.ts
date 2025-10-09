6443cf79f1ee2635f2ac55fa572c3d8d17c08c1b
gustavonunesx <gu.nunesx@gmail.com>
2025-10-09 14:22:47 -0300

    att

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from 'next'
 
+const isGithubPages = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: isGithubPages ? '/ATIEX-FRONT' : '',
+  assetPrefix: isGithubPages ? '/ATIEX-FRONT/' : '',
   images: {
     unoptimized: true
-  },
-
+  }
 }
 
 export default nextConfig
